fc618864f8677de571c83fe1c6b632ef9d7084da
chaipat <chaipat.trong@gmail.com>
2024-09-09 15:31:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Casousel from './components/casousel.component';
+
+function App() {
+let slides = [
+  "https://plus.unsplash.com/premium_photo-1676218968741-8179dd7e533f?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1672115680958-54438df0ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW91bnRhaW5zfGVufDB8fDB8fHww",
+  "https://plus.unsplash.com/premium_photo-1672115680957-a7641352aa49?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1672115680953-bb22e8930a14?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+]
+  return (
+    <div className='w-[100%] m-auto'>
+      <Casousel slides={slides} />
+    </div>
+  );
+}
+
+export default App;
